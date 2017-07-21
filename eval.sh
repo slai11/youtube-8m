31e9ad4fa511248570b00830f37363b732d798d4
OMGWHY <lyanxi2@dso.org.sg>
2017-07-21 15:46:59 +0800

    minor adjustments

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
-MODEL_DIR=model/sq_wavenet_new_fast_reg_small_1024
+MODEL_DIR=model_checkpoints/sq_wavenet_new_fast_reg_small_1024
 MODEL=compressedResWavenetModel
 
-python eval.py --eval_data_pattern='features/validate*.tfrecord' --frame_features=True --feature_names="rgb" --feature_sizes="1024" --batch_size=64 --model=$MODEL --train_dir=$MODEL_DIR --run_once=True --training=False
+python eval.py --eval_data_pattern='dataset/test_set/validate*.tfrecord' --frame_features=True --feature_names="rgb" --feature_sizes="1024" --batch_size=64 --model=$MODEL --train_dir=$MODEL_DIR --run_once=True --training=False
